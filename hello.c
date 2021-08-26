e7e1ba8db72c2f7739d346786c23967f826364c9
Lindsey Wiegert <wiegertlindsey@gmail.com>
2021-08-26 23:40:40 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Lindsey Wiegert
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in c
+ *
+ */
+    #include <stdlib.h>
+    #include <stdio.h>
+    
+    int main(int argc, char **argv){
+
+    printf("Lindsey wiegert\n")
+    
+    return 0;
+}
